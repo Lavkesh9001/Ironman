3dcb63b453b90959866a0b890996d7aa2b7c8da5
Lavkesh9001 <dhanushgaye@gmail.com>
2024-11-13 21:09:02 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,15 @@
+// JavaScript for image slideshow
+let currentSlide = 0;
+const slides = document.querySelectorAll('.artwork img');
+
+function showSlide(index) {
+    slides.forEach(slide => slide.style.display = 'none');
+    slides[index].style.display = 'block';
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+}
+
+setInterval(nextSlide, 3000); // Change image every 3 seconds
